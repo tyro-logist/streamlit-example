eaac9e5bc75db5ce4cc1862614470b6045000fc1
tyro logist <77165972+tyro-logist@users.noreply.github.com>
2023-01-28 15:24:22 -0500

    test plotting
    co2, year

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+"""import streamlit as st
 import pandas as pd
 
 st.title("Hello World")
@@ -9,4 +9,22 @@ if st.button("Say Hello"):
     
 url = "https://raw.githubusercontent.com/tyro-logist/Ontario-GREENHOUSE-GAS-EMISSIONS-REPORTING-BY-FACILITY/main/GHG_Data_2010_2020_data_Dec162021.csv"
 df = pd.read_csv(url, encoding = "ISO-8859-1")
-df.head()
+df.head()"""
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("CO2e Emissions over Time")
+
+url = "https://raw.githubusercontent.com/tyro-logist/Ontario-GREENHOUSE-GAS-EMISSIONS-REPORTING-BY-FACILITY/main/GHG_Data_2010_2020_data_Dec162021.csv"
+data = pd.read_csv(url)
+
+st.write("Data loaded from:", url)
+
+# Create line plot
+plt.plot(data['Year'], data['Total CO2e from all sources in CO2e (t)'])
+plt.xlabel('Year')
+plt.ylabel('Total CO2e from all sources in CO2e (t)')
+st.pyplot()
+
+
