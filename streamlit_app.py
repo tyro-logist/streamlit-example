1009b07e1e9c5de4d460efd89bd7c35ed5a320b4
tyro logist <77165972+tyro-logist@users.noreply.github.com>
2023-01-28 15:16:37 -0500

    test pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Hello World")
 st.write("This is a Streamlit app that says 'Hello World'.")
 
 if st.button("Say Hello"):
     st.write("Hello World!")
+    
+url = "https://raw.githubusercontent.com/tyro-logist/Ontario-GREENHOUSE-GAS-EMISSIONS-REPORTING-BY-FACILITY/main/GHG_Data_2010_2020_data_Dec162021.csv"
+df = pd.read_csv(url, encoding = "ISO-8859-1")
+df.head()
